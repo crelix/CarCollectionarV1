5b115b6cfb87263aaa6d1b0b89f28cb5146f09b6
crelix <djcrelix@inbox.lv>
2016-03-09 22:04:06 +0200

    Update mainClass.java

--- src/Clicker/mainClass.java
+++ src/Clicker/mainClass.java
@@ -4,23 +4,27 @@ import javax.swing.JFrame;
 
 public class mainClass {
 
-	
-//	MAIN KLASE KUR TIEK IZVEIDOTS LOGS PRIEKS SPELES
 	 public static void main(String[] args) {
 	    	
 		 ClickerGame game = new ClickerGame();
-		 
 
 	        JFrame frame = new JFrame("CarCollectionarV1");
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 	      
 	        game.addComponentToPane(frame.getContentPane());
 
 	        frame.pack();
 	        frame.setVisible(true);
 	        frame.setResizable(true);
-	    	frame.setSize(1000, 700);  
+	    	frame.setSize(1000, 700);
+	    	
+	    	LoadFile load = new LoadFile();
+	 
+	 Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+
+	        public void run() {
+	        SaveFile save = new SaveFile();
+	        }
+	    }));
 	 }
-	
 }
