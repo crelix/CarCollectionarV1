331fee9c926e36929da273a55f0a9f30d885d13f
crelix <djcrelix@inbox.lv>
2016-03-09 22:10:19 +0200

    Create PartHolder.java

---
+++ src/Clicker/PartHolder.java
@@ -0,0 +1,284 @@
+package Clicker;
+
+public class PartHolder {
+
+	
+	public String parts[] = new String[260];
+	
+	public PartHolder(){
+		
+		
+	}
+	
+	public void PartsMaker(String a1, int k, String a2, int k1){
+		
+		parts[k] = a1;
+		parts[k1] = a2;
+		
+	}
+	
+	public void MakeParts(){
+		
+		int u = 0; int i = 1;
+		PartsMaker("2", u, "0", i); // Accessories Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Body Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Brakes Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Cooling Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Electronics Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Engine Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Exaust Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Fuel Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Interior Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Steering Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Suspension Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Transmission Level 1
+		u += 2; i +=2;
+		PartsMaker("2", u, "0", i); // Tires Level 1
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Accessories Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Body Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Brakes Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Cooling Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Electronics Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Engine Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Exaust Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Fuel Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Interior Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Steering Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Suspension Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Transmission Level 2
+		u += 2; i +=2;
+		PartsMaker("3", u, "0", i); // Tires Level 2
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Accessories Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Body Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Brakes Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Cooling Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Electronics Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Engine Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Exaust Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Fuel Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Interior Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Steering Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Suspension Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Transmission Level 3
+		u += 2; i +=2;
+		PartsMaker("4", u, "0", i); // Tires Level 3
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Accessories Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Body Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Brakes Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Cooling Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Electronics Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Engine Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Exaust Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Fuel Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Interior Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Steering Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Suspension Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Transmission Level 4
+		u += 2; i +=2;
+		PartsMaker("5", u, "0", i); // Tires Level 4
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Accessories Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Body Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Brakes Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Cooling Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Electronics Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Engine Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Exaust Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Fuel Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Interior Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Steering Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Suspension Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Transmission Level 5
+		u += 2; i +=2;
+		PartsMaker("6", u, "0", i); // Tires Level 5
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Accessories Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Body Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Brakes Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Cooling Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Electronics Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Engine Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Exaust Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Fuel Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Interior Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Steering Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Suspension Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Transmission Level 6
+		u += 2; i +=2;
+		PartsMaker("7", u, "0", i); // Tires Level 6
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Accessories Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Body Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Brakes Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Cooling Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Electronics Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Engine Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Exaust Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Fuel Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Interior Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Steering Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Suspension Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Transmission Level 7
+		u += 2; i +=2;
+		PartsMaker("8", u, "0", i); // Tires Level 7
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Accessories Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Body Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Brakes Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Cooling Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Electronics Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Engine Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Exaust Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Fuel Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Interior Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Steering Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Suspension Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Transmission Level 8
+		u += 2; i +=2;
+		PartsMaker("9", u, "0", i); // Tires Level 8
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Accessories Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Body Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Brakes Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Cooling Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Electronics Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Engine Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Exaust Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Fuel Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Interior Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Steering Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Suspension Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Transmission Level 9
+		u += 2; i +=2;
+		PartsMaker("10", u, "0", i); // Tires Level 9
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Accessories Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Body Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Brakes Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Cooling Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Electronics Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Engine Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Exaust Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Fuel Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Interior Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Steering Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Suspension Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Transmission Level 10
+		u += 2; i +=2;
+		PartsMaker("11", u, "0", i); // Tires Level 10
+		u += 2; i +=2;
+	}
+}
