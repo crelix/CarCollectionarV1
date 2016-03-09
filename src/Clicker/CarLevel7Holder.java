5ece661ca5ccec7f63f66fc939009458b8ab6b66
crelix <djcrelix@inbox.lv>
2016-03-09 22:08:51 +0200

    Create CarLevel7Holder.java

---
+++ src/Clicker/CarLevel7Holder.java
@@ -0,0 +1,87 @@
+package Clicker;
+
+public class CarLevel7Holder {
+
+	public String cars[] = new String[80];
+
+	public CarLevel7Holder(){
+		
+		
+	}
+	
+	public void CarsMaker(String a1,int k, String a2,int k1, String a3,int k2,String a4,int k3){
+
+		cars[k] = a1;
+		cars[k1] = a2;
+		cars[k2] = a3;
+		cars[k3] = a4;
+	}
+		
+		public void MakeCars(){
+			
+			int u = 0;int i = 1;int o = 2;int p = 3;
+			CarsMaker("Type", u,"Lamborghini", i,"Italian",o, "0",p);       //Lamborghini_Aventador
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Nissan", i,"Japanese", o,"0",p);        	//Nissan_350z
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Jaguar", i,"British", o,"0",p);          	//Jaguar_XJ6
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Renault", i,"French",o,"0",p);         	//Renault_Clio
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Toyota", i,"Japanese", o,"0",p);    		//Toyota_Avensis
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Subaru", i,"Japanese", o,"0",p);    	 	//Subaru_Outback
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Saab", i,"Swedish", o,"0",p);		 		//Saab_900
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Opel", i,"Germany", o,"0",p);  			//Opel_Vectra	
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Chrysler", i,"American", o,"0",p);    		//Chrysler_Charger
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Ferrari", i,"Italian", o,"0",p);  		 	//Ferrari_348
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Hyundai", i,"Korean", o,"0",p); 			//Hyundai_XG	
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Lexus", i,"Japanese", o,"0",p); 			//Lexus_RX
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Dodge", i,"American", o,"0",p);    		//Dodge_400
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"GMC", i,"American", o,"0",p);    			//GMC_Terrain
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Volvo", i,"Swedish", o,"0",p);  			//Volvo_C70
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Ford", i,"American", o,"0",p);    			//Ford_Sierra	
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Subaru", i,"Japanese", o,"0",p);  			//Subaru_Impreza
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"BMW", i,"Germany", o,"0",p);           	//BMW_e90	
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Nissan", i,"Japanese", o,"0",p); 			//Nissan_Skyline
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Audi", i,"Germany", o,"0",p); 				//Audi_Q7
+			u += 4; i += 4; o +=4; p+= 4;
+			
+	}
+		
+		 	//CarsLv7 Lamborghini_Aventador = new CarsLv7("Type", "Lamborghini", "Italian", 0);
+		    //CarsLv7 Nissan_350z = new CarsLv7("Type", "Nissan", "Japanese", 0);
+		    //CarsLv7 Jaguar_XJ6 = new CarsLv7("Type", "Jaguar", "British", 0);
+		    //CarsLv7 Renault_Clio = new CarsLv7("Type", "Renault", "French", 0);
+		    //CarsLv7 Toyota_Avensis = new CarsLv7("Type", "Toyota", "Japanese", 0);
+		    //CarsLv7 Subaru_Outback = new CarsLv7("Type", "Subaru", "Japanese", 0);
+		    //CarsLv7 Saab_900 = new CarsLv7("Type", "Saab", "Swedish", 0);
+		    //CarsLv7 Opel_Vectra = new CarsLv7("Type", "Opel", "Germany", 0);
+		    //CarsLv7 Chrysler_Charger = new CarsLv7("Type", "Chrysler", "American", 0);
+		    //CarsLv7 Ferrari_348 = new CarsLv7("Type", "Ferrari", "Italian", 0);
+		    //CarsLv7 Hyundai_XG = new CarsLv7("Type", "Hyundai", "Korean", 0);
+		    //CarsLv7 Lexus_RX = new CarsLv7("Type", "Lexus", "Japanese", 0);
+		    //CarsLv7 Dodge_400 = new CarsLv7("Type", "Dodge", "American", 0);
+		    //CarsLv7 GMC_Terrain = new CarsLv7("Type", "GMC", "American", 0);
+		    //CarsLv7 Volvo_C70 = new CarsLv7("Type", "Volvo", "Swedish", 0);
+		    //CarsLv7 Ford_Sierra = new CarsLv7("Type", "Ford", "American", 0);
+		    //CarsLv7 Subaru_Impreza = new CarsLv7("Type", "Subaru", "Japanese", 0);
+		    //CarsLv7 BMW_e90 = new CarsLv7("Type", "BMW", "Germany", 0);
+		    //CarsLv7 Nissan_Skyline = new CarsLv7("Type", "Nissan", "Japanese", 0);
+		    //CarsLv7 Audi_Q7 = new CarsLv7("Type", "Audi", "Germany", 0);
+
+}
