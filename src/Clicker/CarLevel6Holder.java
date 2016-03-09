01a61f1bee105dde1ca2e35c7d0538348d7faae5
crelix <djcrelix@inbox.lv>
2016-03-09 22:08:36 +0200

    Create CarLevel6Holder.java

---
+++ src/Clicker/CarLevel6Holder.java
@@ -0,0 +1,87 @@
+package Clicker;
+
+public class CarLevel6Holder {
+
+	public String cars[] = new String[80];
+
+	public CarLevel6Holder(){
+		
+		
+	}
+	
+	public void CarsMaker(String a1,int k, String a2,int k1, String a3,int k2,String a4,int k3){
+
+		cars[k] = a1;
+		cars[k1] = a2;
+		cars[k2] = a3;
+		cars[k3] = a4;
+	}
+		
+		public void MakeCars(){
+			
+			int u = 0;int i = 1;int o = 2;int p = 3;
+			CarsMaker("Type", u,"Rolls_Royce", i,"British",o, "0",p);       //Rolls_Royce_Phantom
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Toyota", i,"Japanese", o,"0",p);        	//Toyota_86
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"BMW", i,"Germany", o,"0",p);          	 	//BMW_e70
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Chrysler", i,"American",o,"0",p);         	//Chrysler_Voyager
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Rolls_Royce", i,"British", o,"0",p);    	//Rolls_Royce_PhantomIII
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Honda", i,"Japanese", o,"0",p);    	 	//Honda_CRX
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Subaru", i,"Japanese", o,"0",p);		 	//Subaru_1000
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Cadilliac", i,"American", o,"0",p);  		//Cadilliac_Eldorado
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Land_Rover", i,"British", o,"0",p);    	//Land_Rover_Freelander
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Renault", i,"French", o,"0",p);  		 	//Renault_Spider
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Gaz", i,"Russian", o,"0",p); 				//Gaz_21	
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Acura", i,"Japanese", o,"0",p); 			//Acura_EL
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Bugatti", i,"French", o,"0",p);    		//Bugatti_Veyron
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Mercedes_Benz", i,"Germany", o,"0",p);    	//Mercedes_Benz_SL600
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Rolls_Royce", i,"British", o,"0",p);  		//Rolls_Royce_Silver
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Mini", i,"British", o,"0",p);    			//Mini_Paceman
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Hyundai", i,"Korean", o,"0",p);  			//Hyundai_Excel
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Gaz", i,"Russian", o,"0",p);           	//Gaz_24	
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Peugeot", i,"French", o,"0",p); 			//Peugeot_107
+			u += 4; i += 4; o +=4; p+= 4;
+			CarsMaker("Type", u,"Audi", i,"Germany", o,"0",p); 				//Audi_A4
+			u += 4; i += 4; o +=4; p+= 4;
+			
+	}
+		
+		 	//CarsLv6 Rolls_Royce_Phantom = new CarsLv6("Type", "Rolls_Royce", "British", 0);
+		    //CarsLv6 Toyota_86 = new CarsLv6("Type", "Toyota", "Japanese", 0);
+		    //CarsLv6 BMW_e70 = new CarsLv6("Type", "BMW", "Germany", 0);
+		    //CarsLv6 Chrysler_Voyager = new CarsLv6("Type", "Chrysler", "American", 0);
+		    //CarsLv6 Rolls_Royce_PhantomIII = new CarsLv6("Type", "Rolls_Royce", "British", 0);
+		    //CarsLv6 Honda_CRX = new CarsLv6("Type", "Honda", "Japanese", 0);
+		    //CarsLv6 Subaru_1000 = new CarsLv6("Type", "Subaru", "Japanese", 0);
+		    //CarsLv6 Cadilliac_Eldorado = new CarsLv6("Type", "Cadilliac", "American", 0);
+		    //CarsLv6 Land_Rover_Freelander = new CarsLv6("Type", "Land_Rover", "British", 0);
+		    //CarsLv6 Renault_Spider = new CarsLv6("Type", "Renault", "French", 0);
+		    //CarsLv6 Gaz_21 = new CarsLv6("Type", "GAZ", "Russian", 0);
+		    //CarsLv6 Acura_EL = new CarsLv6("Type", "Acura", "Japanese", 0);
+		    //CarsLv6 Bugatti_Veyron = new CarsLv6("Type", "Bugatti", "French", 0);
+		    //CarsLv6 Mercedes_Benz_SL600 = new CarsLv6("Type", "Mercedes_Benz", "Germany", 0);
+		    //CarsLv6 Rolls_Royce_Silver = new CarsLv6("Type", "Rolls_Royce", "British", 0);
+		    //CarsLv6 Mini_Paceman = new CarsLv6("Type", "Mini", "British", 0);
+		    //CarsLv6 Hyundai_Excel = new CarsLv6("Type", "Hyundai", "Korean", 0);
+		    //CarsLv6 Gaz_24 = new CarsLv6("Type", "GAZ", "Russian", 0);
+		    //CarsLv6 Peugeot_107 = new CarsLv6("Type", "Peugeot", "French", 0);
+		    //CarsLv6 Audi_A4 = new CarsLv6("Type", "Audi", "Germany", 0);
+
+}
